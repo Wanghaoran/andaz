1b845d46ef5473a78fbbe110233f4ea3a347034a
WangHaoRan <whrlmc@139.com>
2014-12-23 16:20:38 +0800

    123123

--- application/views/index.php
+++ application/views/index.php
@@ -26,7 +26,36 @@
 
 
     </script>
-    <script src="http://www.dodoca.com/www/mobile/mods/js/music.js?v=1.3" type="text/javascript"></script>
+    <script>
+        var is_open = 'on';
+        window.onload=function(){
+//            var id = document.getElementById("loading");
+//            setTimeout(function(){document.body.removeChild(id)},1000);
+            if($("#audio_btn").attr("url").indexOf("mp3")>1){
+                var url = $("#audio_btn").attr("url");
+                var auto = is_open=='on' ? 'autoplay' : '';
+                var html = '<audio loop  src="'+url+'" id="media" '+auto+' ></audio>';
+                setTimeout(function(){
+                    $("#audio_btn").html(html);
+                    $("#audio_btn").show().attr("class",is_open);
+                },500);
+
+                $("#audio_btn").on('touchstart',function(){
+                    var type = $("#audio_btn").attr("class");
+                    var media = $("#media").get(0);
+                    if(type=="on"){
+                        media.pause();
+                        $("#audio_btn").attr("class","off");
+                    }else{
+                        media.play();
+                        $("#audio_btn").attr("class","on");
+                    }
+                })
+            }
+
+        }
+
+    </script>
 </head>
 <body>
 
